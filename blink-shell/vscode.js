82bc48392e03fc743fa87bd19de9f348b13c85eb
podobongbong <kim11560@snu.ac.kr>
2020-06-25 17:45:27 +0900

    Changed colors

--- blink-shell/vscode.js
+++ blink-shell/vscode.js
@@ -1,4 +1,4 @@
-t.prefs_.set('color-palette-overrides',["#1c1c1c", "#f44747", "#608b4e", "#d7ba7d", "#223e55", "#646695", "#4ec9b0", "#cfcfcf", "#808080", "#d16969", "#b5cea8", "#dcdcaa", "#569cd6", "#c586c0", "#9cdcfe", "#ececec"]);
-t.prefs_.set('foreground-color', "#ececec");
-t.prefs_.set('background-color', "#1c1c1c");
+t.prefs_.set('color-palette-overrides',["#1e1e1e", "#f44747", "#6a9955", "#d7ba7d", "#6796e6", "#646695", "#569cd6", "#808080", "#333333", "#ce9178", "#b5cea8", "#e6d2aa", "#8daaeb", "#8486ac", "#9cdcfe", "#d4d4d4"]);
+t.prefs_.set('foreground-color', "#d4d4d4");
+t.prefs_.set('background-color', "#1e1e1e");
 t.prefs_.set('cursor-color', 'rgba(187,187,187,0.5)');
